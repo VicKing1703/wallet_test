c7bd7b7e0286428a42a713a27285de6dc4605468
Михаил Горелов <gorelov.m@it-webdevelopment.com>
2025-08-03 10:13:40 +0300

    икс

--- src/test/java/com/uplatform/wallet_tests/api/redis/exception/RedisClientException.java
+++
@@ -1,7 +0,0 @@
-package com.uplatform.wallet_tests.api.redis.exception;
-
-public class RedisClientException extends RuntimeException {
-    public RedisClientException(String message) {
-        super(message);
-    }
-}
