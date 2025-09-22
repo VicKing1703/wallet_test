457bad330ed59ce276a09d160584c0daa337c31c
gorelov-m-v <123184098+gorelov-m-v@users.noreply.github.com>
2025-09-22 23:02:41 +0300

    refactor: simplify redis properties with lombok

--- kafka-api/src/main/java/com/uplatform/wallet_tests/api/redis/config/RedisInstanceProperties.java
+++ kafka-api/src/main/java/com/uplatform/wallet_tests/api/redis/config/RedisInstanceProperties.java
@@ -1,7 +1,10 @@
 package com.uplatform.wallet_tests.api.redis.config;
 
+import lombok.Data;
+
 import java.time.Duration;
 
+@Data
 public class RedisInstanceProperties {
 
     private String host;
@@ -11,100 +14,12 @@ public class RedisInstanceProperties {
     private Duration timeout = Duration.ofSeconds(60);
     private LettucePoolProperties lettucePool = new LettucePoolProperties();
 
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public int getDatabase() {
-        return database;
-    }
-
-    public void setDatabase(int database) {
-        this.database = database;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Duration getTimeout() {
-        return timeout;
-    }
-
-    public void setTimeout(Duration timeout) {
-        this.timeout = timeout;
-    }
-
-    public LettucePoolProperties getLettucePool() {
-        return lettucePool;
-    }
-
-    public void setLettucePool(LettucePoolProperties lettucePool) {
-        this.lettucePool = lettucePool;
-    }
-
+    @Data
     public static class LettucePoolProperties {
         private Integer maxActive;
         private Integer maxIdle;
         private Integer minIdle;
         private Duration maxWait;
         private Duration shutdownTimeout = Duration.ofMillis(100);
-
-        public Integer getMaxActive() {
-            return maxActive;
-        }
-
-        public void setMaxActive(Integer maxActive) {
-            this.maxActive = maxActive;
-        }
-
-        public Integer getMaxIdle() {
-            return maxIdle;
-        }
-
-        public void setMaxIdle(Integer maxIdle) {
-            this.maxIdle = maxIdle;
-        }
-
-        public Integer getMinIdle() {
-            return minIdle;
-        }
-
-        public void setMinIdle(Integer minIdle) {
-            this.minIdle = minIdle;
-        }
-
-        public Duration getMaxWait() {
-            return maxWait;
-        }
-
-        public void setMaxWait(Duration maxWait) {
-            this.maxWait = maxWait;
-        }
-
-        public Duration getShutdownTimeout() {
-            return shutdownTimeout;
-        }
-
-        public void setShutdownTimeout(Duration shutdownTimeout) {
-            this.shutdownTimeout = shutdownTimeout;
-        }
     }
 }
-
--- kafka-api/src/main/java/com/uplatform/wallet_tests/api/redis/config/RedisModuleProperties.java
+++ kafka-api/src/main/java/com/uplatform/wallet_tests/api/redis/config/RedisModuleProperties.java
@@ -1,31 +1,20 @@
 package com.uplatform.wallet_tests.api.redis.config;
 
 import com.uplatform.wallet_tests.config.RedisAggregateConfig;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+
+@Data
 public class RedisModuleProperties {
 
     private RedisAggregateConfig aggregate;
     private Map<String, RedisClientProperties> clients = new LinkedHashMap<>();
 
-    public RedisAggregateConfig getAggregate() {
-        return aggregate;
-    }
-
-    public void setAggregate(RedisAggregateConfig aggregate) {
-        this.aggregate = aggregate;
-    }
-
-    public Map<String, RedisClientProperties> getClients() {
-        return clients;
-    }
-
-    public void setClients(Map<String, RedisClientProperties> clients) {
-        this.clients = clients;
-    }
-
+    @Data
+    @EqualsAndHashCode(callSuper = true)
     public static class RedisClientProperties extends RedisInstanceProperties {
     }
 }
-
